78ee7aad76450ad4a6d5b085649fb9db35c6f533
Kiwicase <caseydentprojects@gmail.com>
2023-04-06 10:20:35 +0300

    Combined 2 arrays in spread.js

--- 1 - Store Multiple Values in an Array/1 - What is an Array_/start/js/spread.js
+++ 1 - Store Multiple Values in an Array/1 - What is an Array_/start/js/spread.js
@@ -1,4 +1,8 @@
 const middle = ['lettuce', 'cheese', 'patty'];
 const burger = ['top bun', ...middle, 'bottom bun'];
 
-console.log(burger);
+const brass = ['trumpet', 'trombone', 'french horn', 'baritone', 'tube'];
+const woodwind = ['flute', 'oboe', 'clarinet', 'saxophone', 'bassoon'];
+const instruments = [...brass, ...woodwind];
+
+console.log(instruments);
