c8d60bccb9ed60bc24a35e4d4fdf07c57e9991db
Kiwicase <caseydentprojects@gmail.com>
2023-04-06 10:26:36 +0300

    New array added to spread.js

--- 1 - Store Multiple Values in an Array/1 - What is an Array_/start/js/spread.js
+++ 1 - Store Multiple Values in an Array/1 - What is an Array_/start/js/spread.js
@@ -4,5 +4,8 @@ const burger = ['top bun', ...middle, 'bottom bun'];
 const brass = ['trumpet', 'trombone', 'french horn', 'baritone', 'tube'];
 const woodwind = ['flute', 'oboe', 'clarinet', 'saxophone', 'bassoon'];
 const instruments = [...brass, ...woodwind];
+brass.push('flugelhorn');
 
-console.log(instruments);
+const numbers = [10, 20, 30, 40]
+
+console.log(Math.max(numbers));
