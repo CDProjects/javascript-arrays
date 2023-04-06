1834f5b2c0f505b9096d53fafc9ba39d390860f8
Kiwicase <caseydentprojects@gmail.com>
2023-04-06 11:47:55 +0300

    Amended indexOf in search-list.js

--- 1 - Store Multiple Values in an Array/1 - What is an Array_/start/js/search-list.js
+++ 1 - Store Multiple Values in an Array/1 - What is an Array_/start/js/search-list.js
@@ -3,9 +3,9 @@ const search = prompt('Search for a product.');
 let message;
 
 if (inStock.includes(search)) {
-    message = `Yes, we have <strong>${search}</strong>.`;
+    message = `Yes, we have <strong>${search}</strong>. It's ${inStock.indexOf(search)} on the list!`;
 } else {
-    message = `Sorry, we do not have <strong>${search}</strong>. It's ${inStock.indexOf(search)} on the list!`;
+    message = `Sorry, we do not have <strong>${search}</strong>.`;
 
 }
 
