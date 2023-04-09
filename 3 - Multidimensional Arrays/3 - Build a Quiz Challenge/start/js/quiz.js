9a929090034710ae4b6d436ca681dee27f09b7b2
Kiwicase <caseydentprojects@gmail.com>
2023-04-09 15:48:03 +0300

    Answers added to quiz.js

--- 3 - Multidimensional Arrays/3 - Build a Quiz Challenge/start/js/quiz.js
+++ 3 - Multidimensional Arrays/3 - Build a Quiz Challenge/start/js/quiz.js
@@ -1,18 +1,13 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
-const questions = [ 
-  [
-    "How many sides does a square have?"
-  ],
-  [
-    "How many sides does a triangle have?"
-  ],
-  [
-    "How many sides does a hexagon have?"
-  ]
+const questions = [
+  ["How many sides does a square have?"],
+  ["How many sides does a triangle have?"],
+  ["How many sides does a hexagon have?"],
 ];
 
-// 2. Store the number of questions answered correctly
+const answers = [[4], [3], [6]];
 
+// 2. Store the number of questions answered correctly
 
 /* 
   3. Use a loop to cycle through each question
@@ -21,6 +16,5 @@ const questions = [
       - If the response matches the answer, the number of correctly
         answered questions increments by 1
 */
-
 
 // 4. Display the number of correct answers to the user
